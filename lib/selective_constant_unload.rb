479777bf4c54c7d2a3977018697f9cad02602104
Roman Le Negrate <roman.lenegrate@gmail.com>
2008-11-21 11:51:24 +0100

    FutureReference is not needed anymore

--- lib/selective_constant_unload.rb
+++ lib/selective_constant_unload.rb
@@ -2,12 +2,8 @@ module SelectiveConstantUnload
   def self.apply!
     [DispatcherPatch, DependenciesPatch].each &:apply!
   end
-  
-  # Patches
+
   autoload :DispatcherPatch,    'selective_constant_unload/dispatcher_patch'
   autoload :DependenciesPatch,  'selective_constant_unload/dependencies_patch'
-  
-  # Support
-  autoload :FutureReference,  'selective_constant_unload/future_reference'
-  autoload :LoadedFile,       'selective_constant_unload/loaded_file'
+  autoload :LoadedFile,         'selective_constant_unload/loaded_file'
 end
