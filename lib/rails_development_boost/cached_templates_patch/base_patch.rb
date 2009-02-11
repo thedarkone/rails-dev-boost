2cd1b738dac9e58ea7650683d95b5ffc8b59a5e4
thedarkone <nobody>
2009-02-11 19:36:14 +0100

    Handle class level view_path changes, this should also avaid some weirdness in ActionMailer.

--- lib/rails_development_boost/cached_templates_patch/base_patch.rb
+++ lib/rails_development_boost/cached_templates_patch/base_patch.rb
@@ -2,6 +2,11 @@ module RailsDevelopmentBoost
   module CachedTemplatesPatch
     module BasePatch
       
+      def initialize_with_view_path_cache_reset(view_paths = [], assigns_for_first_render = {}, controller = nil)
+        initialize_without_view_path_cache_reset(view_paths, assigns_for_first_render, controller)
+        @view_paths.map(&:new_request!)
+      end
+      
       module ClassMethods
         def process_view_paths(value)
           ActionView::PathSet.new(Array(value).map{|path| to_reloading_path(path)})
@@ -13,14 +18,12 @@ module RailsDevelopmentBoost
       end
       
       def self.included(action_view_base)
+        action_view_base.alias_method_chain :initialize, :view_path_cache_reset
+        
         action_view_base.metaclass.send :remove_method, :process_view_paths
         action_view_base.extend ClassMethods
         # convert already loaded view paths
         ActionController::Base.view_paths.map! {|path| action_view_base.to_reloading_path(path)}
-        
-        ActionController::Dispatcher.to_prepare do
-          ActionController::Base.view_paths.each(&:new_request!)
-        end
       end
       
     end
