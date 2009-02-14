704bee0fc0273be1365fa9c3abf9ebefec7f7608
thedarkone <nobody>
2009-02-14 19:49:44 +0100

    Drop template reloading (now merged into core) and be compatible with post #7259baa Rails.

--- lib/rails_development_boost/dispatcher_patch.rb
+++ lib/rails_development_boost/dispatcher_patch.rb
@@ -6,16 +6,24 @@ module RailsDevelopmentBoost
       require 'action_controller/dispatcher'
       ActionController::Dispatcher.class_eval do
         to_prepare { ActiveSupport::Dependencies.unload_modified_files }
-        remove_method :cleanup_application
+        remove_method :reload_application
         include patch
       end
     end
     
     # Overridden.
-    def cleanup_application
-      #ActiveRecord::Base.reset_subclasses if defined?(ActiveRecord)
+    def reload_application
+      # Cleanup the application before processing the current request.
+      # ActiveRecord::Base.reset_subclasses if defined?(ActiveRecord)
+      # ActiveSupport::Dependencies.clear
       ActiveSupport::Dependencies.remove_explicitely_unloadable_constants!
       ActiveRecord::Base.clear_reloadable_connections! if defined?(ActiveRecord)
+
+      # Run prepare callbacks before every request in development mode
+      run_callbacks :prepare_dispatch
+
+      ActionController::Routing::Routes.reload
     end
+    
   end
 end
