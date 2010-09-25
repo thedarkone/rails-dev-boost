712157b11106d1448d0b4fd651356ef9a48fa709
Chalo Fernandez <chalofa@gmail.com>
2010-09-25 03:31:25 -0700

    Don't enable the soft_reload in Rake tasks, to fix the: "method 'cleanup_application' not defined in Class" error

--- init.rb
+++ init.rb
@@ -1,4 +1,6 @@
 if Rails.env.development? || (config.respond_to?(:soft_reload) && config.soft_reload)
-  require 'rails_development_boost'
-  RailsDevelopmentBoost.apply!
+  if ENV['_'].grep(/rake$/).empty?
+    require 'rails_development_boost'
+    RailsDevelopmentBoost.apply!
+  end
 end
