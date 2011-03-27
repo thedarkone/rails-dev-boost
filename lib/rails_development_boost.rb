6fd2a55b1b099cac5568b06c8c17905396c8df40
thedarkone <thedarkone2@gmail.com>
2011-03-27 20:03:05 +0200

    Improve on observers support.

--- lib/rails_development_boost.rb
+++ lib/rails_development_boost.rb
@@ -4,12 +4,13 @@ module RailsDevelopmentBoost
       alias_method :singleton_class, :metaclass unless respond_to?(:singleton_class)
     end
     
-    [DispatcherPatch, DependenciesPatch, ViewHelpersPatch, CachedTemplatesPatch].each &:apply!
+    [DispatcherPatch, DependenciesPatch, ObservablePatch, ViewHelpersPatch, CachedTemplatesPatch].each &:apply!
   end
 
   autoload :DispatcherPatch,      'rails_development_boost/dispatcher_patch'
   autoload :DependenciesPatch,    'rails_development_boost/dependencies_patch'
   autoload :LoadedFile,           'rails_development_boost/loaded_file'
+  autoload :ObservablePatch,      'rails_development_boost/observable_patch'
   autoload :ViewHelpersPatch,     'rails_development_boost/view_helpers_patch'
   autoload :CachedTemplatesPatch, 'rails_development_boost/cached_templates_patch'
   
