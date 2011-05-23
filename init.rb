dc74553d0ee9b385114af13f32a19fa2e06a58e5
thedarkone <thedarkone2@gmail.com>
2011-05-23 16:18:28 +0200

    Fix bundler integration on pre 3.0 Rails versions.

--- init.rb
+++ init.rb
@@ -1,4 +1,2 @@
-if !$rails_rake_task && (Rails.env.development? || (config.respond_to?(:soft_reload) && config.soft_reload))
-  require 'rails_development_boost'
-  RailsDevelopmentBoost.apply!
-end
+require 'rails_development_boost'
+RailsDevelopmentBoost.init!
