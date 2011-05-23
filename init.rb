24f6a6e16f2c1d32606f93083bb0ead8d5c891bc
thedarkone <thedarkone2@gmail.com>
2011-05-23 16:24:32 +0200

    Fix bundler integration on pre 3.0 Rails versions.

--- init.rb
+++ init.rb
@@ -1,4 +1,2 @@
-if Rails.env.development? || (config.respond_to?(:soft_reload) && config.soft_reload)
-  require 'rails_development_boost'
-  RailsDevelopmentBoost.apply!
-end
+require 'rails_development_boost'
+RailsDevelopmentBoost.init!
--- lib/rails-dev-boost.rb
+++ lib/rails-dev-boost.rb
@@ -1,1 +1,2 @@
 require 'rails_development_boost' # require the proper module in case somebody forgets to tell bundler to :require => 'rails_development_boost'
+RailsDevelopmentBoost.init!
